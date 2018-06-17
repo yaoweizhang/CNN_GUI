f63eb11f55ee95f95611455aef8cdbca111c8d5f
yaowei <yaowei@evga.com>
2018-06-17 15:22:35 -0700

    first edition

--- CNN_viewer.py
+++ CNN_viewer.py
@@ -22,11 +22,21 @@ class window(QWidget):
         top_layout.addWidget(file_btn)
         top_layout.addWidget(self.label1)
 
+        #bottom part of window
+        add_btn = QPushButton("Add filter")
+        add_btn.clicked.connect(self.add_filter)
+
+        self.bottom_layout = QHBoxLayout()
+        self.bottom_layout.addWidget(add_btn)
+        self.bottom_layout.addStretch()
+        self.add_filter()
+
 
         #Whole structe
         win_layout = QVBoxLayout()
         win_layout.addLayout(top_layout)
         win_layout.addStretch()
+        win_layout.addLayout(self.bottom_layout)
 
         self.setLayout(win_layout)
         self.setWindowTitle("CNN convolution simulator")
@@ -35,6 +45,50 @@ class window(QWidget):
         fname = QFileDialog.getOpenFileName(self, "open file", ".", "Image files(*.jpg *.gif)")
         self.label1.setPixmap(QPixmap(fname))
         self.pic_path = str(fname)
+
+    def add_filter(self):
+        values = {}
+        grid = QGridLayout()
+        for i in range(1, self.filter_size+1):
+            for j in range(1, self.filter_size+1):
+                input_num = QLineEdit("0")
+                if i == j == (self.filter_size+1)/2:
+                    input_num.setText("1")
+                values[(i, j)] = input_num
+                input_num.setValidator(QDoubleValidator())
+                grid.addWidget(input_num, i, j)
+
+        conv_pic = QLabel("Press submit to show result")
+        conv_pic.setAlignment(Qt.AlignCenter)
+
+        submit_btn = QPushButton("Submit")
+        submit_btn.clicked.connect(lambda state, values=values, conv_pic=conv_pic:\
+                self.submit(values, conv_pic))
+
+        single_filter = QVBoxLayout()
+        single_filter.addLayout(grid)
+        single_filter.addWidget(submit_btn)
+        single_filter.addStretch()
+        single_filter.addWidget(conv_pic)
+
+        self.bottom_layout.addLayout(single_filter)
+
+    def submit(self, values, conv_pic):
+        if self.pic_path == None:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Please choose a picture")
+            msg.exec_()
+        else:
+            for i in range(1, self.filter_size+1):
+                for j in range(1, self.filter_size+1):
+                    self.input_filter[i-1][j-1] = float(str(values[(i, j)].text()))
+
+            #print(self.pic_path)
+            #print(self.input_filter)
+
+            new_path = convolution.compute(self.pic_path, self.input_filter)
+            conv_pic.setPixmap(QPixmap(new_path))
 
 
 def main():
--- convolution.py
+++ convolution.py
@@ -1,2 +1,22 @@
-import numpy
+import os
+import numpy as np
 import cv2
+
+def compute(pic_path, filter_mat):
+    im = cv2.imread(pic_path)
+    im_s, fil_s = list(im.shape), list(filter_mat.shape)
+    res = np.zeros((im_s[0]-fil_s[0]+1, im_s[1]-fil_s[1]+1, im_s[2]), dtype=np.uint8)
+
+    for i in range(res.shape[0]):
+        for j in range(res.shape[1]):
+            for k in range(res.shape[2]):
+                tmp = im[i: (i+fil_s[0]), j:(j+fil_s[1]), k] * filter_mat
+                res[i][j][k] = max(min(255, int(round(np.sum(tmp)))), 0)
+
+    cache_dir = os.path.abspath(os.path.join(__file__, "..", "cache"))
+    if not os.path.isdir(cache_dir):
+        os.makedirs(cache_dir)
+    new_path = os.path.join(cache_dir, "tmp.jpg")
+
+    cv2.imwrite(new_path, res)
+    return new_path
